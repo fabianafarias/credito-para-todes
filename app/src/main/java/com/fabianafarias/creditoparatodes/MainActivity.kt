2eeddce2cfa5643ebf7198451a1c7560ce96cab2
fabiana farias <farias.fabianam@gmail.com>
2021-06-11 20:12:37 -0300

    Criando uma segunda activity e fazendo alterações no layout da activity main.

--- app/src/main/java/com/fabianafarias/creditoparatodes/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/creditoparatodes/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.fabianafarias.creditoparatodes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun openIntent(view: View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+
 }
+
+
