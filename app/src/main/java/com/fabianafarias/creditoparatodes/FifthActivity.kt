43a3c7cb466b5df1308575dac92f904f9514d352
fabiana farias <farias.fabianam@gmail.com>
2021-06-12 22:41:03 -0300

    Fazendo modificações no layout e criando a lógica dos radio buttons.

--- app/src/main/java/com/fabianafarias/creditoparatodes/FifthActivity.kt
+++ app/src/main/java/com/fabianafarias/creditoparatodes/FifthActivity.kt
@@ -4,36 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_fifth.*
+
 
 class FifthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fifth)
+
     }
 
-    fun onRadioAutomovelClicked(view: View){
-        val intent = Intent(this, FifthActivity::class.java)
-        startActivity(intent)
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val checked = view.isChecked
+
+            when (view.getId()) {
+                R.id.radio_automovel ->
+                    if (checked) {
+                        radio_automovel.isChecked
+                    }
+                R.id.radio_imovel ->
+                    if (checked) {
+                        radio_imovel.isChecked
+                    }
+                R.id.radio_sem_renda ->
+                    if (checked) {
+                        radio_sem_renda.isChecked
+                    }
+            }
+        }
     }
 
-    fun onRadioImovelClicked(view: View){
-        val intent = Intent(this, FifthActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun onRadioSemRendaClicked(view: View){
-        val intent = Intent(this, FifthActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun sickBarClicked(view: View){
-        val intent = Intent(this, FifthActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun cadastrarOnClicked(view: View) {
+    fun contratarOnClicked(view: View) {
         val intent = Intent(this, SixthActivity::class.java)
         startActivity(intent)
     }
-
 }
+
+
+
+
+
+
--- app/src/main/java/com/fabianafarias/creditoparatodes/SixthActivity.kt
+++ app/src/main/java/com/fabianafarias/creditoparatodes/SixthActivity.kt
@@ -2,10 +2,21 @@ package com.fabianafarias.creditoparatodes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 
 class SixthActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sixth)
+
+
+
+
+
     }
+
 }
+
+
